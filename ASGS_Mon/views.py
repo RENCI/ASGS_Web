3e85e00c90f2b7a5116f8fce727aaaf47d552bd1
Phil Owen <phillipsowen@outlook.com>
2018-05-01 14:06:25 -0400

    implementation of data model and returning data to client

--- ASGS_Mon/views.py
+++ ASGS_Mon/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from random import randint
+from random import randint 
 
-from time import gmtime, strftime
+from ASGS_Mon import models
 
 # main entry point
 def index(request):
@@ -11,106 +11,48 @@ def index(request):
 
 # client-side event request handler
 def event(request):
+
     # compile the info to send back   
-    data = \
-    'data: {"sites" : \
-                [{ \
-                    "site" : "RENCI", \
-                    "info" : \
-                    { \
-                        "rnd" : "' + str(randint(0,3)) + '", \
-                        "name" : "RENCI", \
-                        "type" : "weather | hurricane", \
-                        "datetime" : "' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '", \
-                        "hurricane" : \
+    data = 'data: {"sites" : ['
+
+    for e in models.Event.objects.raw('select e.id AS ''id'', s.name AS ''site_name'', mt.name AS ''message_type_name'', e.event_ts AS ''ts'', s.cluster_name AS ''cluster_name'' \
+                                ,m.advisory_id AS ''advisory_id'', m.message AS ''message_text'', m.storm_name AS ''storm_name'', s.nodes AS ''total_nodes'', CAST(e.nodes_in_use AS varchar(10)) AS ''nodes_in_use'' \
+                                ,m.storm_number AS ''storm_number'', m.other AS ''other'', et.name AS ''event_type_name'', e.nodes_available AS ''nodes_available'' \
+                                from ASGS_Mon_event e \
+                                join ASGS_Mon_message m on m.id=e.message_id \
+                                join ASGS_Mon_message_type_lu mt on mt.id=m.message_type_id \
+                                join ASGS_Mon_site_lu s on s.id=e.site_id \
+                                join ASGS_Mon_event_type_lu et on et.id=e.event_type_id') :        
+        data += '{ \
+                        "site" : "' + e.site_name + '", \
+                        "info" : \
                         { \
-                            "advisory" : "123", \
-                            "other" : "other" \
-                        }, \
-                        "weather" :  \
-                        { \
-                            "message" : "This message is to let you know that the ASGS has been ACTIVATED using NAM forcing on the GRIDFILE mesh." \
+                            "rnd" : ' + str(randint(0,3)) + ', \
+                            "cluster_name" : "' + e.cluster_name + '", \
+                            "nodes" : ' + str(e.total_nodes) + ', \
+                            "nodes_in_use" : ' + str(e.nodes_in_use) + ', \
+                            "nodes_available" : ' + str(e.nodes_available) + ', \
+                            "type" : "' + str(e.event_type_name) + '", \
+                            "datetime" : "' + str(e.ts) + '", \
+                            "hurricane" : \
+                            { \
+                                "storm" : "' + e.storm_name + '", \
+                                "storm_number" : "' + e.storm_number + '", \
+                                "advisory_number" : "' + e.advisory_id + '" \
+                            }, \
+                            "weather" :  \
+                            { \
+                                "message" : "' + e.message_text + '", \
+                                "message_type" : "' + str(e.message_type_name) + '" \
+                            } \
                         } \
-                    } \
-                }, \
-                { \
-                    "site" : "TACC", \
-                    "info" : \
-                    { \
-                        "rnd" : "' + str(randint(0,3)) + '", \
-                        "name" : "TACC", \
-                        "type" : "weather | hurricane", \
-                        "datetime" : "' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '", \
-                        "hurricane" : \
-                        { \
-                            "advisory" : "765", \
-                            "other" : "other" \
-                        }, \
-                        "weather" :  \
-                        { \
-                            "message" : "This message is to let you know that the ASGS has been ACTIVATED using NAM forcing on the GRIDFILE mesh." \
-                        } \
-                    } \
-                }, \
-                { \
-                    "site" : "LSU", \
-                    "info" : \
-                    { \
-                        "rnd" : "' + str(randint(0,3)) + '", \
-                        "name" : "LSU", \
-                        "type" : "weather | hurricane", \
-                        "datetime" : "' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '", \
-                        "hurricane" : \
-                        { \
-                            "advisory" : "65A", \
-                            "other" : "other" \
-                        }, \
-                        "weather" :  \
-                        { \
-                            "message" : "This message is to let you know that the ASGS has been ACTIVATED using NAM forcing on the GRIDFILE mesh." \
-                        } \
-                    } \
-                }, \
-                { \
-                    "site" : "UCF", \
-                    "info" : \
-                    { \
-                        "rnd" : "' + str(randint(0,3)) + '", \
-                        "name" : "UCF", \
-                        "type" : "weather | hurricane", \
-                        "datetime" : "' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '", \
-                        "hurricane" : \
-                        { \
-                            "advisory" : "456", \
-                            "other" : "other" \
-                        }, \
-                        "weather" :  \
-                        { \
-                            "message" : "This message is to let you know that the ASGS has been ACTIVATED using NAM forcing on the GRIDFILE mesh." \
-                        } \
-                    } \
-                }, \
-                { \
-                    "site" : "George Mason", \
-                    "info" : \
-                    { \
-                        "rnd" : "' + str(randint(0,3)) + '", \
-                        "name" : "GM", \
-                        "type" : "weather | hurricane", \
-                        "datetime" : "' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '", \
-                        "hurricane" : \
-                        { \
-                            "advisory" : "789", \
-                            "other" : "other" \
-                        }, \
-                        "weather" :  \
-                        { \
-                            "message" : "This message is to let you know that the ASGS has been ACTIVATED using NAM forcing on the GRIDFILE mesh." \
-                        } \
-                    } \
-                } \
-                ] \
-            } \n\n'
+                  },'
+    
+    # remove the trailing comma
+    data = data[:-1]
+    
+    # finish off the request
+    data += '] } \n\n'
     
     # compile the response
     response = HttpResponse(data, content_type='text/event-stream')
--- Docs/general_testing.sql
+++ Docs/general_testing.sql
@@ -1,17 +1,55 @@
 
-select 'ASGS', e.*, m.*, s.name, et.name, mt.name
-from ASGS_Mon_event e 
-join ASGS_Mon_message m on m.id=e.message_id
-join ASGS_Mon_message_type_lu mt on mt.id=m.message_type_id
-join ASGS_Mon_site_lu s on s.id=e.site_id
-join ASGS_Mon_event_type_lu et on et.id=e.event_type_id
-
+select e.id AS 'id', s.name AS 'site_name', mt.name AS 'message_type_name', e.event_ts AS 'ts', s.cluster_name AS 'cluster_name'
+                                ,m.advisory_id AS 'advisory_id', m.message AS 'message_text', m.storm_name AS 'storm_name'
+                                ,m.storm_number AS 'storm_number', m.other AS 'other', et.name AS 'event_type_name'
+                                from ASGS_Mon_event e
+                                join ASGS_Mon_message m on m.id=e.message_id
+                                join ASGS_Mon_message_type_lu mt on mt.id=m.message_type_id 
+                                join ASGS_Mon_site_lu s on s.id=e.site_id 
+                                join ASGS_Mon_event_type_lu et on et.id=e.event_type_id
+                                
 /*
+delete from ASGS_Mon_event;
+delete from ASGS_Mon_message;
+
 INSERT 
 INTO ASGS_Mon_event (id, event_ts, nodes_in_use, nodes_available, raw_data, event_type_id, message_id, site_id) 
-VALUES (0, date('now'), 100, 200, 'this is raw_data', 0, 0, 0);
+VALUES (0, datetime(), 100, 200, 'this is raw_data0', 0, 0, 0);
+
+INSERT 
+INTO ASGS_Mon_event (id, event_ts, nodes_in_use, nodes_available, raw_data, event_type_id, message_id, site_id) 
+VALUES (1, datetime(), 100, 200, 'this is raw_data1', 0, 1, 1);
+
+INSERT 
+INTO ASGS_Mon_event (id, event_ts, nodes_in_use, nodes_available, raw_data, event_type_id, message_id, site_id) 
+VALUES (2, datetime(), 100, 200, 'this is raw_data2', 0, 2, 2);
+
+INSERT 
+INTO ASGS_Mon_event (id, event_ts, nodes_in_use, nodes_available, raw_data, event_type_id, message_id, site_id) 
+VALUES (3, datetime(), 100, 200, 'this is raw_data3', 0, 3, 3);
+
+INSERT 
+INTO ASGS_Mon_event (id, event_ts, nodes_in_use, nodes_available, raw_data, event_type_id, message_id, site_id) 
+VALUES (4, datetime(), 100, 200, 'this is raw_data4', 0, 4, 4);
+
 
 INSERT 
 INTO ASGS_Mon_message (id, advisory_id, storm_name, storm_number, message, other, message_type_id) 
-VALUES (0, 'Advisory 1', 'TheBigOne', '1', 'This is a big storm', '', 0);
+VALUES (0, 'Advisory 0', 'TheBigOne0', '0', 'This is a big storm0', '', 0);
+
+INSERT 
+INTO ASGS_Mon_message (id, advisory_id, storm_name, storm_number, message, other, message_type_id) 
+VALUES (1, 'Advisory 1', 'TheBigOne1', '1', 'This is a big storm1', '', 1);
+
+INSERT 
+INTO ASGS_Mon_message (id, advisory_id, storm_name, storm_number, message, other, message_type_id) 
+VALUES (2, 'Advisory 2', 'TheBigOne2', '2', 'This is a big storm2', '', 2);
+
+INSERT 
+INTO ASGS_Mon_message (id, advisory_id, storm_name, storm_number, message, other, message_type_id) 
+VALUES (3, 'Advisory 3', 'TheBigOne3', '3', 'This is a big storm3', '', 3);
+
+INSERT 
+INTO ASGS_Mon_message (id, advisory_id, storm_name, storm_number, message, other, message_type_id) 
+VALUES (4, 'Advisory 4', 'TheBigOne4', '4', 'This is a big storm4', '', 4);
 */
