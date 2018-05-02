f37ba30fd5f02ab15d6cb64dabbf9490026dad83
Phil Owen <phillipsowen@outlook.com>
2018-05-02 14:46:49 -0400

    final fixes to get it to work

--- ASGS_Mon/urls.py
+++ ASGS_Mon/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('event/', views.event, name='event'),
+    path('utilview/', views.utilview, name='utilview')
 ]
--- ASGS_Mon/views.py
+++ ASGS_Mon/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
-
+from django.template.context_processors import request
 from django.http import HttpResponse
-from random import randint 
 
+from random import randint 
 from ASGS_Mon import models
 
 # main entry point
 def index(request):
     return render(request, 'ASGS_Mon/index.html', {})
 
+def utilview(request):
+    return render(request, 'ASGS_Mon/utilview.html', {})
+
 # client-side event request handler
 def event(request):
 
     # compile the info to send back   
     data = 'data: {"sites" : ['
-
+    utilization = '';
+    
     for e in models.Event.objects.raw('select e.id AS ''id'', s.name AS ''site_name'', mt.name AS ''message_type_name'', e.event_ts AS ''ts'', s.cluster_name AS ''cluster_name'' \
-                                ,m.advisory_id AS ''advisory_id'', m.message AS ''message_text'', m.storm_name AS ''storm_name'', s.nodes AS ''total_nodes'', CAST(e.nodes_in_use AS varchar(10)) AS ''nodes_in_use'' \
+                                ,m.advisory_id AS ''advisory_id'', m.message AS ''message_text'', m.storm_name AS ''storm_name'', s.nodes AS ''total_nodes'', e.nodes_in_use AS ''nodes_in_use'' \
                                 ,m.storm_number AS ''storm_number'', m.other AS ''other'', et.name AS ''event_type_name'', e.nodes_available AS ''nodes_available'' \
                                 from ASGS_Mon_event e \
                                 join ASGS_Mon_message m on m.id=e.message_id \
@@ -49,11 +53,21 @@ def event(request):
                         } \
                   },'
     
-    # remove the trailing comma
+        available = randint(randint(0, e.total_nodes), e.total_nodes)
+        remaining = e.total_nodes - available
+        
+        #utilization += '{"title" : "' + e.site_name  + '", "subtitle" : "' + e.cluster_name + '", "ranges" : [0, 0, 4000], "measures" : [' + str(e.nodes_in_use) + ', ' + str(e.nodes_available) + '], "markers" : [' + str(e.total_nodes) + ']},'
+        utilization += '{"title" : "' + e.site_name  + '", "subtitle" : "' + e.cluster_name + '", "ranges" : [0, 0, 4000], "measures" : [' + str(available) + ', ' + str(remaining) + '], "markers" : [' + str(e.total_nodes) + ']},'
+
+    # remove the trailing commas
     data = data[:-1]
+    utilization = utilization[:-1]
+    
+    #add in the utilization
+    data += '], "utilization" : [' + utilization + ']'
     
     # finish off the request
-    data += '] } \n\n'
+    data += '} \n\n'
     
     # compile the response
     response = HttpResponse(data, content_type='text/event-stream')
