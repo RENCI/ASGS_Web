7db61cc345309b5ec3832bb65714ebd6d74a8b90
Phil Owen <phillipsowen@outlook.com>
2018-05-02 15:07:21 -0400

    updating with better demo data

--- Docs/Insert_lu_records.sql
+++ Docs/Insert_lu_records.sql
@@ -1,8 +1,8 @@
 INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (0, 'RENCI', '', 'Hatteras', 1024, '', 'NC');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (1, 'TACC', '', '', 1024, '', 'TX');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (2, 'LSU', '', '', 1024, '', 'LA');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (3, 'UCF', '', '', 1024, '', 'FL');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (4, 'GM', '', '', 1024, '', 'VA');
+INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (1, 'TACC', '', 'TACC cluster', 3900, '', 'TX');
+INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (2, 'LSU', '', 'LSU cluster', 2048, '', 'LA');
+INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (3, 'UCF', '', 'UCF cluster', 500, '', 'FL');
+INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, nodes, tech_contact, location) VALUES (4, 'GM', '', 'GM cluster', 2900, '', 'VA');
 
 INSERT INTO ASGS_Mon_message_type_lu (id, name, description) VALUES (0, 'Startup', '');
 INSERT INTO ASGS_Mon_message_type_lu (id, name, description) VALUES (1, 'Info', '');
