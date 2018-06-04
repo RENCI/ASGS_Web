6af9f93a760821d7855464ab283d1f4e2b437a3a
Phil Owen <phillipsowen@outlook.com>
2018-06-04 09:51:17 -0400

    moving to new DB schema

--- ASGS_Mon/models.py
+++ ASGS_Mon/models.py
@@ -5,39 +5,34 @@ class Site_lu(models.Model):
     name = models.CharField(max_length = 50)
     description = models.CharField(max_length = 100)
     cluster_name = models.CharField(max_length = 100)
-    nodes = models.IntegerField()
     tech_contact = models.CharField(max_length = 100)
-    location = models.CharField(max_length = 100)
+    phys_location = models.CharField(max_length = 100)
+    status = models.CharField(max_length = 100)
+    
+class Event_group(models.Model):    
+    id = models.AutoField(primary_key=True)
+    event_group_ts = models.DateTimeField()
+    site = models.ForeignKey(Site_lu, on_delete=models.PROTECT)
+    storm_name = models.CharField(max_length = 50)
+    storm_number = models.CharField(max_length = 50)
+    status = models.CharField(max_length = 100)
+    advisory_id = models.CharField(max_length = 50)
+    final_product = models.CharField(max_length = 1000)
     
 class Event_type_lu(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length = 50)
     description = models.CharField(max_length = 100)
+    view_order = models.IntegerField()
 
-class Message_type_lu(models.Model):    
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 100)
-
-class Message(models.Model):
-    id = models.AutoField(primary_key=True)
-    message_type = models.ForeignKey(Message_type_lu, on_delete=models.PROTECT)
-    advisory_id = models.CharField(max_length = 50)
-    storm_name = models.CharField(max_length = 50)
-    storm_number = models.CharField(max_length = 50)
-    message = models.CharField(max_length = 250)
-    process = models.CharField(max_length = 100)
-    pctcomplete = models.DecimalField(max_digits=100, decimal_places=2)
-    state = models.CharField(max_length = 10)
-    other = models.CharField(max_length = 250)
-    
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     site = models.ForeignKey(Site_lu, on_delete=models.PROTECT)
+    event_group = models.ForeignKey(Event_group, on_delete=models.PROTECT)
     event_type = models.ForeignKey(Event_type_lu, on_delete=models.PROTECT)
-    message = models.ForeignKey(Message, on_delete=models.PROTECT)
     event_ts = models.DateTimeField()
-    nodes_in_use = models.IntegerField()
-    nodes_available = models.IntegerField()
+    advisory_id = models.CharField(max_length = 50)
+    pct_complete = models.FloatField()
+    host_start_file = models.CharField(max_length = 1000)
     raw_data = models.CharField(max_length = 4000)
 
