85f2dad7a8dea93c7ede10b0931be471edc4fc81
Lisa Stillwell <lisa@renci.org>
2018-07-23 10:18:59 -0400

    updated Events and States and handling of those according to 7/10/18 meeting with Phil and Brian

--- receive_msg_service.py
+++ receive_msg_service.py
@@ -13,14 +13,27 @@ logging.basicConfig(filename=logfile, format='%(asctime)s %(message)s', datefmt=
 logging.info("Started receive_msg_service")
 
 # set up AMQP credentials and connect to asgs queue
-credentials = pika.PlainCredentials('user', 'password')
+credentials = pika.PlainCredentials('asgs', 'ZippityD0Da')
 parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials, socket_timeout=2)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
 channel.queue_declare(queue='asgs_queue')
 
-def insert_event(conn, site_id, event_group_id, event_type_id, msg_obj):
+
+# get the correct percent complete for an event type
+def get_pctcomplete(conn, event_type_id):
+
+        query = "SELECT pct_complete FROM ASGS_Mon_event_type_lu WHERE id=" + str(event_type_id)
+        cur = conn.cursor()
+        cur.execute(query)
+        event_lu = cur.fetchone()
+        pct_complete  = event_lu[0]
+       
+        return pct_complete
+    
+
+def insert_event(conn, site_id, event_group_id, event_type_id, state_type, msg_obj):
 
         sql_fields = "INSERT INTO ASGS_Mon_event ("
         sql_values = " VALUES ("
@@ -42,16 +55,27 @@ def insert_event(conn, site_id, event_group_id, event_type_id, msg_obj):
             sql_values += "'N/A', "
 
         sql_fields += "advisory_id, "
-        if (msg_obj.get("advisory_id") is not None and len(msg_obj["advisory_id"]) > 0):
-            sql_values += "'" + msg_obj["advisory_id"] + "', "
+        if (msg_obj.get("advisory_number") is not None and len(str(msg_obj["advisory_number"])) > 0):
+            sql_values += "'" + str(msg_obj["advisory_number"]) + "', "
         else:
             sql_values += "'N/A', "
 
+
+        # for now (until detail page is developed)
+        # ignore intermediary cluster job completion percentage
         sql_fields += "pct_complete, "
-        pctcomplete = "0"
-        if (msg_obj.get("pctcomplete") is not None and len(msg_obj["pctcomplete"]) > 0):
-            pctcomplete = msg_obj["pctcomplete"]
-        sql_values += pctcomplete + ", "
+        pct_complete = get_pctcomplete(conn, event_type_id)
+
+        if ((state_type =="CMPL") or (int(pct_complete) < 20)):
+            sql_values += str(pct_complete) + ", "
+        else: #this should still be the previous pct amount - 20 less - until completion of the event
+            sql_values += str(int(pct_complete)-20) + ", "
+                
+        #pctcomplete = "0"
+        #if (msg_obj.get("pctcomplete") is not None and len(msg_obj["pctcomplete"]) > 0):
+            #pctcomplete = msg_obj["pctcomplete"]
+        #sql_values += pctcomplete + ", "
+
 
         sql_fields += "process, "
         if (msg_obj.get("process") is not None and len(msg_obj["process"]) > 0):
@@ -76,6 +100,8 @@ def insert_event(conn, site_id, event_group_id, event_type_id, msg_obj):
         # add to message table
         sql_stmt = sql_fields + sql_values
         conn.execute(sql_stmt)
+   
+        logging.info(" Inserted event record: " + sql_stmt)
 
 
 def insert_event_group(conn, site_id, state_id, msg_obj):
@@ -97,20 +123,20 @@ def insert_event_group(conn, site_id, state_id, msg_obj):
             sql_values += "'N/A', "
  
         sql_fields += "storm_name, "
-        if (msg_obj.get("storm_name") is not None and len(msg_obj["storm_name"]) > 0):
-            sql_values += "'" + msg_obj["storm_name"] + "', "
+        if (msg_obj.get("storm") is not None and len(msg_obj["storm"]) > 0):
+            sql_values += "'" + msg_obj["storm"] + "', "
         else:
             sql_values += "'N/A', "
 
         sql_fields += "storm_number, "
-        if (msg_obj.get("storm_number") is not None and len(msg_obj["storm_number"]) > 0):
-            sql_values += "'" + msg_obj["storm_number"] + "', "
+        if (msg_obj.get("storm_number") is not None and len(str(msg_obj["storm_number"])) > 0):
+            sql_values += "'" + str(msg_obj["storm_number"]) + "', "
         else:
             sql_values += "'N/A', "
 
         sql_fields += "advisory_id, "
-        if (msg_obj.get("advisory_id") is not None and len(msg_obj["advisory_id"]) > 0):
-            sql_values += "'" + msg_obj["advisory_id"] + "', "
+        if (msg_obj.get("advisory_number") is not None and len(str(msg_obj["advisory_number"])) > 0):
+            sql_values += "'" + str(msg_obj["advisory_number"]) + "', "
         else:
             sql_values += "'N/A', "
 
@@ -121,6 +147,8 @@ def insert_event_group(conn, site_id, state_id, msg_obj):
         sql_stmt = sql_fields + sql_values
         cur = conn.cursor()
         cur.execute(sql_stmt)
+
+        logging.info(" Inserted event group record: " + sql_stmt)
 
         return cur.lastrowid
 
@@ -161,35 +189,56 @@ def callback(ch, method, properties, body):
         cur.execute(query)
         event_lu = cur.fetchone()
         event_type_id = event_lu[0]
+
+        state_name = ""
+        if (msg_obj.get("state") is not None and len(msg_obj["state"]) > 0):
+            state_name = msg_obj["state"]
+        else:
+            logging.error("NO STATE TYPE PROVIDED - must drop message!")
+            return
         
         event_group_id = -1
+        state_id = -1
         # if this is the start of a group of Events, create a new event_group record
         # qualifying group initiation: event type = RSTR
-        if(event_name == "RSTR"):
+        # STRT & HIND do not belong to any event group??
+        # For now it is required that every event belong to an event group, so I will add those as well.
+
+        # ***********THIS SHOULD CHANGE LATER***************
+        # get run state id from state name
+        query = "SELECT id FROM ASGS_Mon_state_type_lu WHERE name='" + state_name + "'"
+        cur = conn.cursor()
+        cur.execute(query)
+        state = cur.fetchone()
+        state_id = state[0]
+
+        if((event_name == "STRT") and (state_name == "INIT")):
             # get run state id from state name
-            state_name = ""
-            if (msg_obj.get("state") is not None and len(msg_obj["state"]) > 0):
-                state_name = msg_obj["state"]
-            else:
-                logging.error("NO STATE ID PROVIDED - must drop message!")
-                return
-            query = "SELECT id FROM ASGS_Mon_state_type_lu WHERE name='" + state_name + "'"
-            cur = conn.cursor()
-            cur.execute(query)
-            state = cur.fetchone()
-            state_id = site[0]
+            #query = "SELECT id FROM ASGS_Mon_state_type_lu WHERE name='" + state_name + "'"
+            #cur = conn.cursor()
+            #cur.execute(query)
+            #state = cur.fetchone()
+            #state_id = state[0]
             event_group_id = insert_event_group(conn, site_id, state_id, msg_obj)
         else:
+            # don't need a new event group
             # get last message from this site in order to retrieve current event group id
             query = "SELECT max(id) AS id, event_group_id FROM ASGS_Mon_event e WHERE e.site_id=" + str(site_id)
             cur = conn.cursor()
             cur.execute(query)
             event = cur.fetchone()
             event_group_id = event[1] 
+            # update event group with this latest state
+            sql_stmt = "UPDATE ASGS_Mon_event_group SET state_type_id = " + str(state_id) + " WHERE id = " + str(event_group_id)
+            conn.execute(sql_stmt)
+
+        # update site with latest state_type_id
+        sql_stmt = "UPDATE ASGS_Mon_site_lu SET state_type_id = " + str(state_id) + " WHERE id = " + str(site_id)
+        conn.execute(sql_stmt)
 
 
         # now insert message into the event table
-        insert_event(conn, site_id, event_group_id, event_type_id, msg_obj)
+        insert_event(conn, site_id, event_group_id, event_type_id, state_name, msg_obj)
      
         # now commit and save
         conn.commit()
