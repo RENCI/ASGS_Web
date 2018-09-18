8b6ab55aa333cbb423bbe929da915b232096bbcd
Lisa Stillwell <lisa@asgs-monitordb.edc.renci.org>
2018-09-18 14:03:54 -0400

    added version 2 models definition

---
+++ ASGS_Mon/modelsv2.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+class State_type_lu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    view_order = models.IntegerField()
+
+class Instance_state_type_lu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    
+class Event_type_lu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    view_order = models.IntegerField()
+    pct_complete = models.IntegerField()
+    
+class Site_lu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    cluster_name = models.CharField(max_length = 100)
+    tech_contact = models.CharField(max_length = 100)
+    phys_location = models.CharField(max_length = 100)
+    notes = models.CharField(max_length = 1000)
+    
+class Event_group(models.Model):    
+    id = models.AutoField(primary_key=True)
+    instance = models.ForeignKey(Instance, on_delete=models.PROTECT)
+    state_type = models.ForeignKey(State_type_lu, on_delete=models.PROTECT)
+    event_group_ts = models.DateTimeField()
+    storm_name = models.CharField(max_length = 50)
+    storm_number = models.CharField(max_length = 50)
+    advisory_id = models.CharField(max_length = 50)
+    final_product = models.CharField(max_length = 1000)
+
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    site = models.ForeignKey(Site_lu, on_delete=models.PROTECT)
+    event_group = models.ForeignKey(Event_group, on_delete=models.PROTECT)
+    event_type = models.ForeignKey(Event_type_lu, on_delete=models.PROTECT)
+    event_ts = models.DateTimeField()
+    advisory_id = models.CharField(max_length = 50)
+    pct_complete = models.IntegerField()
+    process = models.CharField(max_length = 100)
+    raw_data = models.CharField(max_length = 4000)
+
+class Instance(models.Model):
+    id = models.AutoField(primary_key=True)
+    epoch = models.BigIntegerField()
+    run_params = models.CharField(max_length = 100)
+    inst_state_type = models.ForeignKey(Instance_state_type_lu, on_delete=models.PROTECT)
+    site = models.ForeignKey(Site_lu, on_delete=models.PROTECT)
+
+    class Meta:
+       unique_together = (('id', 'epoch'),)
