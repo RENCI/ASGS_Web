345c2eb40f1f1d6c56820c70125b78452de55fb6
Phil Owen <phillipsowen@outlook.com>
2018-09-20 10:26:56 -0400

    added new tables to the admin site

--- ASGS_Mon/admin.py
+++ ASGS_Mon/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Site_lu, Event_type_lu, Event, Event_group, State_type_lu
+from .models import Site_lu, Event_type_lu, Event, Event_group, State_type_lu,Instance_state_type_lu,Instance
 
 admin.site.register(State_type_lu)
+admin.site.register(Instance_state_type_lu)
 admin.site.register(Event_type_lu)
 admin.site.register(Site_lu)
+admin.site.register(Instance)
 admin.site.register(Event_group)
 admin.site.register(Event)
