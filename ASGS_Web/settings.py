2a8208420d306652629f4a8632c791f250263d91
Lisa Stillwell <lisa@asgs-monitor.renci.org>
2018-09-20 10:36:07 -0400

    updated model to connect to postgres db

--- ASGS_Web/settings.py
+++ ASGS_Web/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'ASGS_Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'asgs_testdb',
+        'USER': 'asgstest',
+        'PASSWORD': 'Z1ppityD0Da',
+        'HOST': 'asgs-monitordb.edc.renci.org',
+        'PORT': '',
     }
 }
 
--- Docs/Insert_lu_records.sql
+++ Docs/Insert_lu_records.sql
@@ -1,50 +1,51 @@
-delete from ASGS_Mon_site_lu;
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, tech_contact, phys_location) VALUES (0, 'RENCI', '', 'Hatteras', '', 'NC');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, tech_contact, phys_location) VALUES (1, 'TACC', '', 'TACC cluster', '', 'TX');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, tech_contact, phys_location) VALUES (2, 'LSU', '', 'LSU cluster', '', 'LA');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, tech_contact, phys_location) VALUES (3, 'UCF', '', 'UCF cluster', '', 'FL');
-INSERT INTO ASGS_Mon_site_lu (id, name, description, cluster_name, tech_contact, phys_location) VALUES (4, 'George Mason', '', 'GM cluster', '', 'VA');
+delete from "ASGS_Mon_site_lu";
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (0, 'RENCI', '', 'Hatteras', '', 'NC');
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (1, 'TACC', '', 'TACC cluster', '', 'TX');
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (2, 'LSU', '', 'LSU cluster', '', 'LA');
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (3, 'UCF', '', 'UCF cluster', '', 'FL');
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (4, 'George Mason', '', 'GM cluster', '', 'VA');
+INSERT INTO "ASGS_Mon_site_lu" (id, name, description, cluster_name, tech_contact, phys_location) VALUES (5, 'Penguin', '', 'POD', '', 'Penguin');
 
-delete from ASGS_Mon_event_type_lu;
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (0, 'RSTR', 'New run has started', 0);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (1, 'PRE1', 'Pre-run 1 operations in progress', 20);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (2, 'NOWC', 'NowCast operations in progress', 40);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (3, 'PRE2', 'Pre-run 2 operations in progress', 60);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (4, 'FORE', 'Forcast operations in progress', 80);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (5, 'POST', 'Post-run operations in progress', 90);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (6, 'REND', 'Run has ended', 100);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (7, 'STRT', 'Start submission of run', 0);
-INSERT INTO ASGS_Mon_event_type_lu (id, name, description, pct_complete) VALUES (8, 'HIND', 'Ready for next run', 0);
+delete from "ASGS_Mon_event_type_lu";
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (0, 'RSTR', 'New run has started', 0);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (1, 'PRE1', 'Pre-run 1 operations in progress', 20);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (2, 'NOWC', 'NowCast operations in progress', 40);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (3, 'PRE2', 'Pre-run 2 operations in progress', 60);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (4, 'FORE', 'Forcast operations in progress', 80);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (5, 'POST', 'Post-run operations in progress', 90);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (6, 'REND', 'Run has ended', 100);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (7, 'STRT', 'Start submission of run', 0);
+INSERT INTO "ASGS_Mon_event_type_lu" (id, name, description, pct_complete) VALUES (8, 'HIND', 'Ready for next run', 0);
 
-delete from ASGS_Mon_state_type_lu;
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (0, 'INIT', 'Operational.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (1, 'RUNN', 'Running.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (2, 'PEND', 'Pending.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (3, 'FAIL', 'Error.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (4, 'WARN', 'Warning.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (5, 'IDLE', 'Idle.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (6, 'CMPL', 'Completing an event.');
-INSERT INTO ASGS_Mon_state_type_lu (id, name, description) VALUES (7, 'NONE', 'Starting or completing an event group.');
+delete from "ASGS_Mon_state_type_lu";
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (0, 'INIT', 'Operational.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (1, 'RUNN', 'Running.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (2, 'PEND', 'Pending.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (3, 'FAIL', 'Error.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (4, 'WARN', 'Warning.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (5, 'IDLE', 'Idle.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (6, 'CMPL', 'Completing an event.');
+INSERT INTO "ASGS_Mon_state_type_lu" (id, name, description) VALUES (7, 'NONE', 'Starting or completing an event group.');
 
-delete from ASGS_Mon_instance_state_type_lu;
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (0, 'INIT', 'Operational.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (1, 'RUNN', 'Running.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (2, 'PEND', 'Pending.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (3, 'FAIL', 'Error.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (4, 'WARN', 'Warning.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (5, 'IDLE', 'Idle.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (6, 'CMPL', 'Completing an event.');
-INSERT INTO ASGS_Mon_instance_state_type_lu (id, name, description) VALUES (7, 'NONE', 'Starting or completing an event group.');
+delete from "ASGS_Mon_instance_state_type_lu";
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (0, 'INIT', 'Operational.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (1, 'RUNN', 'Running.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (2, 'PEND', 'Pending.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (3, 'FAIL', 'Error.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (4, 'WARN', 'Warning.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (5, 'IDLE', 'Idle.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (6, 'CMPL', 'Completing an event.');
+INSERT INTO "ASGS_Mon_instance_state_type_lu" (id, name, description) VALUES (7, 'NONE', 'Starting or completing an event group.');
 
 -- use this to create python constants ready for cut/paste into the codebase
 select d.const_name from 
 (
-        select id, '1' as 'theorder', 'CONST_SITE_' || UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from ASGS_Mon_site_lu
+        select id, '1' as 'theorder', 'CONST_SITE_' || UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from "ASGS_Mon_site_lu"
         union
-        select id, '2' as 'theorder', 'CONST_EVENT_TYPE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from ASGS_Mon_event_type_lu
+        select id, '2' as 'theorder', 'CONST_EVENT_TYPE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from "ASGS_Mon_event_type_lu"
         union
-        select id, '3' as 'theorder', 'CONST_STATE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from ASGS_Mon_state_type_lu
+        select id, '3' as 'theorder', 'CONST_STATE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from "ASGS_Mon_state_type_lu"
         union
-        select id, '4' as 'theorder', 'CONST_INSTANCE_STATE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from ASGS_Mon_instance_state_type_lu
+        select id, '4' as 'theorder', 'CONST_INSTANCE_STATE_' ||  UPPER(REPLACE(name, ' ', '') || ' = ' || id) as 'const_name' from "ASGS_Mon_instance_state_type_lu"
 ) as d
 order by d.theorder, d.id
