66382122b4d30f85690eae13fe682284fbb6adce
Lisa Stillwell <lisa@asgs-monitor.renci.org>
2018-09-24 15:04:02 -0400

    update receive message process to use postgres db and add some test messages

---
+++ bin/start_asgs.sh
@@ -0,0 +1,18 @@
+# /usr/bin/bash
+
+#get in the right place
+cd ../
+
+# start the receive messages process
+# this will get asgs messages off of the Rabbit MQ and write them into the ASGS Monitor DB
+setsid python receive_msg_service.py > /dev/null 2>&1 &
+RETVAL=$?
+PID=$!
+[ $RETVAL -eq 0 ] && echo $PID > .msg_pidfile && echo "Started ASGS-Monitor receive mesg process" || echo "Failed to successfully start receive mesg ASGS-Monitor process"
+
+# now run the DJango server with ASGS Monitor website
+setsid /bin/python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
+RETVAL=$?
+PID=$!
+[ $RETVAL -eq 0 ] && echo $PID > .django_pidfile && echo "Started JDango ASGS-Monitor service" || echo "Failed to successfully start JDango ASGS-Monitor service"
+
