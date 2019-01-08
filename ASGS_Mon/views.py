fd64506e434083c5bb3ca197817c14bb6e0ef1b9
Phil Owen <phillipsowen@outlook.com>
2019-01-08 12:56:29 -0500

    getting ready for the config list and config details data transfer.

--- ASGS_Mon/views.py
+++ ASGS_Mon/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from ASGS_Mon import models
     
 # define legal request types
-theLegalReqTypes = ['init', 'event']
+theLegalReqTypes = ['init', 'event', 'config_list', 'config_detail']
 
 # main entry point
 def index(request):
@@ -13,11 +13,19 @@ def index(request):
 def dataReq(request): 
     # get the request type         
     reqType = request.GET.get('type')
-
+    param = request.GET.get('param')
+    
+    # if there was no param passed just init the paramval used to an empty string
+    if param is None: 
+        paramVal = '' 
+    else:
+        paramVal = param
+        
     # only legal commands can pass
     if reqType in theLegalReqTypes:
+        
         # create the SQL. raw SQL calls using the django db model need an ID
-        theSQL = 'SELECT 1 AS ''id'', public.get_' + reqType + '_json() AS ''data'';'
+        theSQL = 'SELECT 1 AS ''id'', public.get_' + reqType + '_json(' + paramVal + ') AS ''data'';'
             
         # get the data
         data = str(models.Json.objects.raw(theSQL)[0].data).replace("'", "\"")
