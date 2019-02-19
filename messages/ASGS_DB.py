6aad70d78c99d3a22a929c9e0fb6699e6b8751cf
Phil Owen <phillipsowen@outlook.com>
2019-02-19 09:53:42 -0500

    adding try catches

--- messages/ASGS_DB.py
+++ messages/ASGS_DB.py
@@ -7,19 +7,24 @@ class ASGS_DB:
     def __init__(self, logger, ASGSConstants_inst, parser):
         self.logger = logger#.getLogger('ASGS_DB')
         
-        #self.logger.debug("Connecting to DB: {0}".format(parser.get('postgres', 'database')))
-        
-        #conn_str = "host={0} port={1} dbname={2} user={3} password={4}".format(parser.get('postgres', 'host'), parser.get('postgres', 'port'), parser.get('postgres', 'database'), parser.get('postgres', 'username'), parser.get('postgres', 'password'))
-
-        #self.conn = psycopg2.connect(conn_str)
-        
-        self.logger.debug("Got a connection to the DB")
-
-        #self.cursor = self.conn.cursor
-                
-        self.logger.debug("ASGS_DB initialized")
-
-        #self.ASGSConstants_inst = ASGSConstants_inst
+        try:
+            self.parser = parser
+            self.ASGSConstants_inst = ASGSConstants_inst
+            
+            #self.logger.debug("Connecting to DB: {0}".format(parser.get('postgres', 'database')))
+            
+            #conn_str = "host={0} port={1} dbname={2} user={3} password={4}".format(parser.get('postgres', 'host'), parser.get('postgres', 'port'), parser.get('postgres', 'database'), parser.get('postgres', 'username'), parser.get('postgres', 'password'))
+    
+            #self.conn = psycopg2.connect(conn_str)
+            
+            self.logger.debug("Got a connection to the DB")
+    
+            #self.cursor = self.conn.cursor
+                    
+            self.logger.debug("ASGS_DB initialized")
+        except:
+            e = sys.exc_info()[0]
+            self.logger.warn("FAILURE initializing. error {0}".format(str(e)))
 
     def __del__(self):
         # now commit and save
