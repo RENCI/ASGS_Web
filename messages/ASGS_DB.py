9874e621730818b2bb1f62e37776f6dc33f597ce
Phil Owen <phillipsowen@outlook.com>
2019-02-19 10:10:57 -0500

    restoring more func

--- messages/ASGS_DB.py
+++ messages/ASGS_DB.py
@@ -47,15 +47,15 @@ class ASGS_DB:
         try:        
             self.logger.debug("sql_stmt: {0}".format(sql_stmt))
             
-            #self.cursor.execute(sql_stmt)
+            self.cursor.execute(sql_stmt)
             
-            #retVal = self.cursor.fetchone()
+            retVal = self.cursor.fetchone()
             
             self.logger.debug("sql_stmt executed")
 
-            #self.cursor.commit()
+            self.cursor.commit()
             
-            return 1#retVal[0]
+            return retVal[0]
         except:
             e = sys.exc_info()[0]
             self.logger.error("FAILURE - DB issue: " + str(e))
@@ -81,5 +81,96 @@ class ASGS_DB:
         self.logger.debug("existing_group_id: {0}".format(existing_group_id))
         
         return existing_group_id
+        
+    ##########################################
+    # just a check to see if there are any instances defined for this site yet
+    ##########################################
+    def get_existing_instance_id(self, site_id , msg_obj):
+        self.logger.debug("site_id: {0}".format(site_id))
     
+        # get the instance name
+        instance_name = msg_obj.get("instance_name", "N/A") if (msg_obj.get("instance_name", "N/A") != "") else "N/A"
+    
+        # get the process id
+        process_id = int(msg_obj.get("uid", "0")) if (msg_obj.get("uid", "0") != "") else 0
+    
+        # see if there are any instances yet that have this site_id and instance_name
+        # this could be caused by a new install that does not have any data in the DB yet
+        sql_stmt = 'SELECT id FROM "ASGS_Mon_instance" WHERE site_id={0} AND process_id={1} AND instance_name=\'{2}\' AND inst_state_type_id!=9 ORDER BY id DESC'.format(site_id, process_id, instance_name)
+                                                     
+        # TODO +++++++++++++++FIX THIS++++++++++++++++++++Add query to get correct stat id for Defunct++++++++++++++++++++++++
+        # TODO +++++++++++++++FIX THIS++++++++++++++++++++Add day to query too? (to account for rollover of process ids)++++++++++++++++++++++++
+        
+        inst = self.exec_sql(sql_stmt)
+
+        if (inst is not None):
+            existing_instance_id = inst
+        else:
+            existing_instance_id = -1
+        
+        self.logger.debug("existing_instance_id {0}".format(existing_instance_id))
+                
+        return existing_instance_id
+    
+    
+    ##########################################
+    # gets the instance id for a process
+    ##########################################
+    def get_instance_id(self, start_ts, site_id, process_id, instance_name):
+        self.logger.debug("start_ts: {0}, site_id: {1}, process_id: {2}, instance_name:{3}".format(start_ts, site_id, process_id, instance_name))
+           
+        sql_stmt = 'SELECT id FROM "ASGS_Mon_instance" WHERE CAST(start_ts as DATE)=\'{0}\' AND site_id={1} AND process_id={2} AND instance_name=\'{3}\''.format(start_ts[:10], site_id, process_id, instance_name)
+                                                 
+        inst = self.exec_sql(sql_stmt)
    
+        if (inst is not None):
+            _id = inst
+        else:
+            _id = -1
+        
+        self.logger.debug("returning id: {0}".format(_id))
+        
+        return id   
+    
+    
+    ##########################################
+    # update the event group
+    ##########################################
+    def update_event_group(self, state_id, event_group_id, msg_obj):
+        # get the storm name
+        storm_name = msg_obj.get("storm", "N/A") if (msg_obj.get("storm", "N/A") != "") else "N/A"
+    
+        # get the advisory id
+        advisory_id = msg_obj.get("advisory_number", "N/A") if (msg_obj.get("advisory_number", "N/A") != "") else "N/A"
+    
+        sql_stmt = 'UPDATE "ASGS_Mon_event_group" SET state_type_id ={0}, storm_name=\'{1}\', advisory_id=\'{2}\' WHERE id={3}'.format(state_id, storm_name, advisory_id, event_group_id)
+        
+        self.exec_sql(sql_stmt)
+    
+    
+    ##########################################
+    # update instance with latest state_type_id
+    ##########################################
+    def update_instance(self, state_id, site_id, instance_id, msg_obj):
+        # get a default time stamp, use it if necessary
+        now = datetime.datetime.now()
+        ts = now.strftime("%Y-%m-%d %H:%M")
+        end_ts = msg_obj.get("date-time", ts) if (msg_obj.get("date-time", ts) != "") else ts
+    
+        # get the run params
+        run_params = msg_obj.get("run_params", "N/A") if (msg_obj.get("run_params", "N/A") != "") else "N/A"
+    
+        sql_stmt = 'UPDATE "ASGS_Mon_instance" SET inst_state_type_id = {0}, end_ts = \'{1}\', run_params = \'{2}\' WHERE site_id = {3} AND id={4}'.format(state_id, end_ts, run_params, site_id, instance_id)
+                                                   
+        self.exec_sql(sql_stmt)
+   
+    ##########################################
+    # saves the raw message
+    ##########################################
+    def save_raw_msg(self, msg):
+        self.logger.debug("msg: {0}".format(msg))
+    
+        sql_stmt = 'INSERT INTO "ASGS_Mon_json" (data) VALUES (\'{0}\''.format(msg)
+        
+        self.exec_sql(sql_stmt)
+        
