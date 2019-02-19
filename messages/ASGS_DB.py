c721e00ee9368fa2cfa2e1a04c594b727ddbb76d
Phil Owen <phillipsowen@outlook.com>
2019-02-19 09:00:58 -0500

    fixing connection

--- messages/ASGS_DB.py
+++ messages/ASGS_DB.py
@@ -2,26 +2,34 @@ import sys
 import re
 import psycopg2
 import datetime
+from receive_config_message_service import db_connect
 
 class ASGS_DB:
     def __init__(self, logger, ASGSConstants_inst, parser):
         self.logger = logger
         self.ASGSConstants_inst = ASGSConstants_inst
+        
         self.parser = parser
+        
+        self.conn = db_connect()
+        
+        self.cursor = self.conn.cursor
         
         logger.debug("ASGS_DB initialized")
 
-#    def __del__(self):
+    def __del__(self):
         # now commit and save
-#        try:
-#            self.logger.info("Committing and closing the connection")
+        try:
+            self.logger.debug("Committing and closing the connection")
             
             # if we got this far commit all outstanding DB queries    
-            #self.db_commit()
-            #self.conn.close()
-#        except:
-#            e = sys.exc_info()[0]
-#            self.logger.warn("FAILURE - Error closing DB connection. error {0}".format(str(e)))
+            self.db_commit()
+            self.conn.close()
+
+            self.logger.debug("Commited and connection closed")
+        except:
+            e = sys.exc_info()[0]
+            self.logger.warn("FAILURE - Error closing DB connection. error {0}".format(str(e)))
 
     ##########################################
     # just a check to see if there are any event groups defined for this site yet
@@ -31,17 +39,12 @@ class ASGS_DB:
     
         # see if there are any event groups yet that have this instance_id
         # this could be caused by a new install that does not have any data in the DB yet
-        query = 'SELECT id FROM "ASGS_Mon_event_group" WHERE instance_id={0} AND advisory_id=\'{1}\' ORDER BY id DESC'.format(instance_id, advisory_id)
+        sql_stmt = 'SELECT id FROM "ASGS_Mon_event_group" WHERE instance_id={0} AND advisory_id=\'{1}\' ORDER BY id DESC'.format(instance_id, advisory_id)
         
-        self.logger.debug("query: {0}".format(query))
-        
-        cur = self.conn.cursor()
-        cur.execute(query)
-        
-        group = cur.fetchone()
+        group = self.exec_sql(sql_stmt)
         
         if (group is not None):
-            existing_group_id = group[0]
+            existing_group_id = group
         else:
             existing_group_id = -1
             
@@ -64,41 +67,20 @@ class ASGS_DB:
     
         # see if there are any instances yet that have this site_id and instance_name
         # this could be caused by a new install that does not have any data in the DB yet
-        query = 'SELECT id FROM "ASGS_Mon_instance" WHERE site_id={0} AND process_id={1} AND instance_name=\'{2}\' AND inst_state_type_id!=9 ORDER BY id DESC'.format(site_id, process_id, instance_name)
+        sql_stmt = 'SELECT id FROM "ASGS_Mon_instance" WHERE site_id={0} AND process_id={1} AND instance_name=\'{2}\' AND inst_state_type_id!=9 ORDER BY id DESC'.format(site_id, process_id, instance_name)
                                                      
         # TODO +++++++++++++++FIX THIS++++++++++++++++++++Add query to get correct stat id for Defunct++++++++++++++++++++++++
         # TODO +++++++++++++++FIX THIS++++++++++++++++++++Add day to query too? (to account for rollover of process ids)++++++++++++++++++++++++
         
-        self.logger.debug("query: {0}".format(query))
-            
-        try:        
-            self.db_connect()
-
-            cur = self.conn.cursor()            
-        except:
-            e = sys.exc_info()[0]
-            self.logger.error("FAILURE - DB issue: " + str(e))
-            return
-
-        self.logger.info("got cursor")
-
-        cur.execute(query)
-            
-        self.logger.info("executed")
-            
-        inst = cur.fetchone()
-            
-        self.logger.info("Received {0}" % inst[0])
+        inst = self.exec_sql(sql_stmt)
 
         if (inst is not None):
-            existing_instance_id = inst[0]
+            existing_instance_id = inst
         else:
             existing_instance_id = -1
         
         self.logger.debug("existing_instance_id {0}".format(existing_instance_id))
-        
-        self.conn.close()
-        
+                
         return existing_instance_id
     
     
@@ -107,17 +89,13 @@ class ASGS_DB:
     ##########################################
     def get_instance_id(self, start_ts, site_id, process_id, instance_name):
         self.logger.debug("start_ts: {0}, site_id: {1}, process_id: {2}, instance_name:{3}".format(start_ts, site_id, process_id, instance_name))
-        
-    
-        query = 'SELECT id FROM "ASGS_Mon_instance" WHERE CAST(start_ts as DATE)=\'{0}\' AND site_id={1} AND process_id={2} AND instance_name=\'{3}\''.format(start_ts[:10], site_id, process_id, instance_name)
+           
+        sql_stmt = 'SELECT id FROM "ASGS_Mon_instance" WHERE CAST(start_ts as DATE)=\'{0}\' AND site_id={1} AND process_id={2} AND instance_name=\'{3}\''.format(start_ts[:10], site_id, process_id, instance_name)
                                                  
-        self.logger.debug("query: " + query)
-        cur = self.conn.cursor()
-        cur.execute(query)
-        inst = cur.fetchone()
-    
+        inst = self.exec_sql(sql_stmt)
+   
         if (inst is not None):
-            _id = inst[0]
+            _id = inst
         else:
             _id = -1
         
@@ -137,10 +115,7 @@ class ASGS_DB:
     
         sql_stmt = 'UPDATE "ASGS_Mon_event_group" SET state_type_id ={0}, storm_name=\'{1}\', advisory_id=\'{2}\' WHERE id={3}'.format(state_id, storm_name, advisory_id, event_group_id)
         
-        self.logger.debug("sql_stmt: {0}".format(sql_stmt))
-                     
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
+        self.exec_sql(sql_stmt)
     
     
     ##########################################
@@ -157,11 +132,8 @@ class ASGS_DB:
     
         sql_stmt = 'UPDATE "ASGS_Mon_instance" SET inst_state_type_id = {0}, end_ts = \'{1}\', run_params = \'{2}\' WHERE site_id = {3} AND id={4}'.format(state_id, end_ts, run_params, site_id, instance_id)
                                                    
-        self.logger.debug("sql: {0}".format(sql_stmt))
-        
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
-    
+        self.exec_sql(sql_stmt)
+   
     ##########################################
     ##########################################
     def save_raw_msg(self, msg):
@@ -169,10 +141,7 @@ class ASGS_DB:
     
         sql_stmt = 'INSERT INTO "ASGS_Mon_json" (data) VALUES (\'{0}\''.format(msg)
         
-        self.logger.debug("sql: {0}".format(sql_stmt))
-        
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
+        self.exec_sql(sql_stmt)
         
     ##########################################
     ##########################################
@@ -203,12 +172,7 @@ class ASGS_DB:
         # create the fields
         sql_stmt = 'INSERT INTO "ASGS_Mon_event" (site_id, event_group_id, event_type_id, event_ts, advisory_id, pct_complete, process{0}) VALUES ({1}, {2}, {3}, \'{4}\', \'{5}\', {6}, \'{7}\'{8})'.format(rawDataCol, site_id, event_group_id, event_type_id, event_ts, advisory_id, pct_complete, process, msg_line)
     
-        self.logger.debug("About to insert event record: {0}".format(sql_stmt))
-        
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
-       
-        self.logger.debug("Inserted event record: {0}".format(sql_stmt))
+        self.exec_sql(sql_stmt)
     
     ##########################################
     ##########################################
@@ -228,15 +192,12 @@ class ASGS_DB:
         advisory_id = msg_obj.get("advisory_number", "N/A") if (msg_obj.get("advisory_number", "N/A") != "") else "N/A"
          
         sql_stmt = 'INSERT INTO "ASGS_Mon_event_group" (state_type_id, instance_id, event_group_ts, storm_name, storm_number, advisory_id, final_product) VALUES ({0}, {1}, \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'product\') RETURNING id'.format(state_id, instance_id, event_group_ts, storm_name, storm_number, advisory_id)
-        
-        self.logger.debug("About to insert event group record: {0}".format(sql_stmt))
-        
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
-    
-        self.logger.debug(" Inserted event group record: {0}".format(sql_stmt))
-    
-        return cur.fetchone()[0]
+
+        group = self.exec(sql_stmt)
+
+        self.logger.debug("group {0}".format(group))
+
+        return group
     
     
     ##########################################
@@ -263,13 +224,33 @@ class ASGS_DB:
     
         sql_stmt = 'INSERT INTO "ASGS_Mon_instance" (site_id, process_id, start_ts, end_ts, run_params, instance_name, inst_state_type_id) VALUES ({0}, {1}, \'{2}\', \'{3}\', \'{4}\', \'{5}\', {6}) RETURNING id'.format(site_id, process_id, start_ts, end_ts, run_params, instance_name, state_id)
     
-        self.logger.debug("About to insert instance record: " + sql_stmt)
-        cur = self.conn.cursor()
-        cur.execute(sql_stmt)
+        inst = self.exec(sql_stmt)
+
+        self.logger.debug("inst {0}".format(inst))
+
+        return inst
     
-        self.logger.debug(" Inserted instance record: " + sql_stmt)
-        return cur.fetchone()[0]
+    ###########################################
+    # executes a sql statement, returns the first row
+    ###########################################
     
+    def exec_sql(self, sql_stmt):
+        try:        
+            self.logger.debug("sql_stmt: {0}".format(sql_stmt))
+            
+            self.cursor.execute(sql_stmt)
+            
+            retVal = self.cursor.fetchone()
+            
+            self.logger.debug("sql_stmt executed")
+
+            return retVal[0]
+        except:
+            e = sys.exc_info()[0]
+            self.logger.error("FAILURE - DB issue: " + str(e))
+            return
+
+        
     ##########################################
     # gets a connection to the DB
     ##########################################
@@ -278,13 +259,7 @@ class ASGS_DB:
         
         conn_str = "host={0} port={1} dbname={2} user={3} password={4}".format(self.parser.get('postgres', 'host'), self.parser.get('postgres', 'port'), self.parser.get('postgres', 'database'), self.parser.get('postgres', 'username'), self.parser.get('postgres', 'password'))
                    
-        self.conn = psycopg2.connect(conn_str)
-        
-        
-    ##########################################
-    # commits all outstanding DB queries
-    ##########################################
-    def db_commit(self):
-        self.conn.commit()
+        return psycopg2.connect(conn_str)
+
 
     
