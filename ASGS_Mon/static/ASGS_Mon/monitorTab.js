cb98a0145778123342ffd33848fe7a9090512efb
Phil Owen <phillipsowen@outlook.com>
2019-02-21 10:49:50 -0500

    fixing event msg request string

--- ASGS_Mon/static/ASGS_Mon/monitorTab.js
+++ ASGS_Mon/static/ASGS_Mon/monitorTab.js
@@ -331,7 +331,7 @@ function renderMonitorTab(siteInstance)
 			});
 			
 			// load the shells for all the site instance events
-			d3.json("dataReq/?type=event" + "&viewActiveFlag=" + viewActiveFlag + "&viewInactiveFlag=" + viewInactiveFlag + "&sinceDate=" + sinceDate + "&sites=" + sites.toString(), function(error, eventData)
+			d3.json("dataReq/?type=event" + "&viewActiveFlag=" + viewActiveFlag + "&inactives=" + inactives.toString() + "&sinceDate=" + sinceDate + "&sites=" + sites.toString(), function(error, eventData)
 			{				
 				// erase all the site instances on error
 				if (error || eventData.length == 0) 
