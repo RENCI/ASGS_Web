8c680d3ee0b5e8cdea69f824e15656460c96e189
Phil Owen <phillipsowen@outlook.com>
2019-02-21 08:25:43 -0500

    debugging

--- messages/receive_msg_service_pg.py
+++ messages/receive_msg_service_pg.py
@@ -15,37 +15,37 @@ if __name__ == "__main__":
     # create a logger
     logger = logging.getLogger(__name__)
 
-    try:                    
+#    try:                    
         # retrieve configuration settings
-        parser = ConfigParser()
-        parser.read('msg_settings.ini')#/srv/django/ASGS_Web/messages/
+    parser = ConfigParser()
+    parser.read('msg_settings.ini')#/srv/django/ASGS_Web/messages/
+
+    logger.info("Configuring ASGS Msg queue.")
+
+    # set up AMQP credentials and connect to asgs queue
+    credentials = pika.PlainCredentials(parser.get('pika', 'username'), parser.get('pika', 'password'))
     
-        # set up AMQP credentials and connect to asgs queue
-        credentials = pika.PlainCredentials(parser.get('pika', 'username'), parser.get('pika', 'password'))
-        
-        parameters = pika.ConnectionParameters(parser.get('pika', 'host'), parser.get('pika', 'port'), '/', credentials, socket_timeout=2)
-        
-        logger.info("Configuring ASGS Msg queue.")
+    parameters = pika.ConnectionParameters(parser.get('pika', 'host'), parser.get('pika', 'port'), '/', credentials, socket_timeout=2)
     
-        connection = pika.BlockingConnection(parameters)
-        
-        channel = connection.channel()    
-        
-        channel.queue_declare(queue='asgs_queue')
-        
-        logger.info("ASGS Msg queue declared.")
-        
-        # get an instance to the callback handler
-        Queue_callback_inst = ASGS_Queue_callback(parser)
-        
-        channel.basic_consume(Queue_callback_inst.callback, queue='asgs_queue', no_ack=True)
-        
-        logger.info('ASGS Msg queue configured and waiting for messages...')
-        
-        channel.start_consuming()
-    except:
-        e = sys.exc_info()[0]
-        logger.error("FAILURE - Problems initiating receive_msg_queue. error {0}".format(str(e)))
+    connection = pika.BlockingConnection(parameters)
+    
+    channel = connection.channel()    
+    
+    channel.queue_declare(queue='asgs_queue')
+    
+    logger.info("ASGS Msg queue declared.")
+    
+    # get an instance to the callback handler
+    Queue_callback_inst = ASGS_Queue_callback(parser)
+    
+    channel.basic_consume(Queue_callback_inst.callback, queue='asgs_queue', no_ack=True)
+    
+    logger.info('ASGS Msg queue configured and waiting for messages...')
+    
+    channel.start_consuming()
+#    except:
+#        e = sys.exc_info()[0]
+#        logger.error("FAILURE - Problems initiating receive_msg_queue. error {0}".format(str(e)))
 
     
     
