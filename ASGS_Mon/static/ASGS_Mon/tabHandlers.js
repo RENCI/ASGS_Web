985ab04d7d79e7974e0410698b42ff0053b0b9c7
Phil Owen <phillipsowen@outlook.com>
2019-02-25 14:29:42 -0500

    adding tooltips...

--- ASGS_Mon/static/ASGS_Mon/tabHandlers.js
+++ ASGS_Mon/static/ASGS_Mon/tabHandlers.js
@@ -98,7 +98,7 @@ function initConfigTabList()
 		configList.forEach(function(info, i)
 		{
 			// tack on each entry
-			output += '<span class="input-group-text">' + info.instance_name + ' - Started on: ' + info.start_ts + '. <a href="javascript:getConfigDetails(' + info.instance_id + ', \'asgs\')">View ASGS</a>&nbsp; or &nbsp;<a href="javascript:getConfigDetails(' + info.instance_id + ', \'adcirc\')">View ADCIRC</a></span><div style="display:none; border:1px solid black; margin-top: 5px; margin-bottom: 5px" id="configdetail_' + info.instance_id + '"></div></br>';
+			output += '<span class="input-group-text">' + info.instance_name + ' - Started on: ' + info.start_ts + '. <a href="javascript:getConfigDetails(' + info.instance_id + ', \'asgs\')">View ASGS</a>&nbsp; or &nbsp;<a href="javascript:getConfigDetails(' + info.instance_id + ', \'adcirc\')">View ADCIRC</a></span><div style="display:none; border:1px solid black; margin-top: 5px; margin-bottom: 5px" id="configdetail_' + info.instance_id + '"></div><br>';
 		});
 		
 		// output the result
