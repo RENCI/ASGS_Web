5b8ad5480085b213830289188f5fc6950c12e744
Phil Owen <phillipsowen@outlook.com>
2019-02-26 12:49:59 -0500

    updating all procs

--- Docs/Databases/PG/SPs.sql
+++ Docs/Databases/PG/SPs.sql
@@ -1,9 +1,49 @@
--- FUNCTION: public.get_event_msgs_by_group_json(integer)
+-- FUNCTION: public.get_config_detail_json(integer)
 
--- DROP FUNCTION public.get_event_msgs_by_group_json(integer);
+-- DROP FUNCTION public.get_config_detail_json(integer);
 
-CREATE OR REPLACE FUNCTION public.get_event_msgs_by_group_json(
-	_event_group_id integer)
+CREATE OR REPLACE FUNCTION public.get_config_detail_json(
+	_instance_id integer)
+    RETURNS TABLE(document json) 
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 1000
+AS $BODY$
+
+BEGIN
+  -- gather the records and return them in json format
+        RETURN QUERY
+
+        SELECT to_json(r)
+        FROM (
+                SELECT c.instance_id, i.instance_name, TO_CHAR(i.start_ts, 'Mon DD, HH24:MI:SS') AS start_ts, c.adcirc_config, REPLACE(REPLACE(REPLACE(c.asgs_config, '\"', '&quot;'), '''', '&apos;'), '\n', '</br>') AS asgs_config
+                FROM public."ASGS_Mon_instance_config" c
+                JOIN "ASGS_Mon_instance" i ON i.id=c.instance_id
+                WHERE c.instance_id=_instance_id
+                GROUP BY c.instance_id, i.instance_name, i.start_ts, c.adcirc_config, c.asgs_config
+                ORDER BY c.instance_id DESC
+                LIMIT 7) r;
+END;
+
+$BODY$;
+
+ALTER FUNCTION public.get_config_detail_json(integer)
+    OWNER TO postgres;
+
+GRANT EXECUTE ON FUNCTION public.get_config_detail_json(integer) TO asgs;
+
+GRANT EXECUTE ON FUNCTION public.get_config_detail_json(integer) TO postgres;
+
+GRANT EXECUTE ON FUNCTION public.get_config_detail_json(integer) TO PUBLIC;
+
+-- FUNCTION: public.get_config_list_json()
+
+-- DROP FUNCTION public.get_config_list_json();
+
+CREATE OR REPLACE FUNCTION public.get_config_list_json(
+	)
     RETURNS TABLE(document jsonb) 
     LANGUAGE 'plpgsql'
 
@@ -14,34 +54,40 @@ AS $BODY$
 
 BEGIN
   -- gather the records and return them in json format
-	RETURN QUERY
-	
-	SELECT jsonb_agg(r)
-	FROM (
-		SELECT (to_char(e.event_ts, 'Mon DD, HH24:MI:SS') || ' - ' || e.raw_data) AS event_summary
-		FROM public."ASGS_Mon_event" e
-		WHERE e.event_group_id=_event_group_id
-		GROUP BY id 
-		ORDER BY id DESC
-		LIMIT 7) r;
-END;
+        RETURN QUERY
+        
+        SELECT jsonb_agg(r)
+        FROM ( 
+                SELECT c.instance_id, i.instance_name, TO_CHAR(i.start_ts, 'Mon DD, HH24:MI:SS') AS start_ts                                
+                FROM public."ASGS_Mon_instance_config" c
+                JOIN "ASGS_Mon_instance" i ON i.id=c.instance_id
+                GROUP BY c.instance_id, i.instance_name, i.start_ts                                
+                ORDER BY c.instance_id DESC
+                LIMIT 7) r;
+END;            
 
 $BODY$;
 
-ALTER FUNCTION public.get_event_msgs_by_group_json(integer)
-    OWNER TO powen;
+ALTER FUNCTION public.get_config_list_json()
+    OWNER TO postgres;
 
-GRANT EXECUTE ON FUNCTION public.get_event_msgs_by_group_json(integer) TO asgs;
+GRANT EXECUTE ON FUNCTION public.get_config_list_json() TO asgs;
 
-COMMENT ON FUNCTION public.get_event_msgs_by_group_json(integer)
-    IS 'Returns a recordset (json) of the last 5 event messages for a group.';
+GRANT EXECUTE ON FUNCTION public.get_config_list_json() TO postgres;
 
--- FUNCTION: public.get_event_json()
+GRANT EXECUTE ON FUNCTION public.get_config_list_json() TO PUBLIC;
 
--- DROP FUNCTION public.get_event_json();
+-- FUNCTION: public.get_event_json(boolean, date, text, text)
+
+-- DROP FUNCTION public.get_event_json(boolean, date, text, text);
 
 CREATE OR REPLACE FUNCTION public.get_event_json(
-	)
+	_viewactive boolean,
+	_since date DEFAULT to_date(
+	'01/01/1970'::text,
+	'MM/DD/YYYY'::text),
+	_inactives text DEFAULT ''::text,
+	_sites text DEFAULT ''::text)
     RETURNS TABLE(document json) 
     LANGUAGE 'plpgsql'
 
@@ -75,7 +121,7 @@ BEGIN
 
 			etl.name AS type,
 			'Storm ' || eg.storm_name || ': ' || etl.description || ' for advisory number ' || eg.advisory_id AS event_operation, 
-			(SELECT public.get_event_msgs_by_group_json(eg.id)) AS event_raw_msgs,
+			(SELECT public.get_event_msgs_by_group_json(eg.id, eg.advisory_id)) AS event_raw_msgs,
 			e.pct_complete AS pct_complete,
 			to_char(e.event_ts, 'YYYY-MM-DD HH24:MI:SS') AS datetime,
 			etl.description AS message,
@@ -90,9 +136,26 @@ BEGIN
 			JOIN "ASGS_Mon_event_type_lu" etl ON etl.id=e.event_type_id
 			JOIN "ASGS_Mon_state_type_lu" stlgrp ON stlgrp.id=eg.state_type_id
 			JOIN "ASGS_Mon_instance_state_type_lu" istlu ON istlu.id=i.inst_state_type_id
-			INNER JOIN (SELECT max(id) AS id from "ASGS_Mon_event" group by event_group_id) AS meid ON meid.id=e.id
-			INNER JOIN (SELECT max(id) AS id, instance_id from "ASGS_Mon_event_group" group by instance_id) AS megid ON megid.id=e.event_group_id AND megid.instance_id=i.id	
-			WHERE inst_state_type_id < CASE WHEN end_ts + INTERVAL '1 day' > now() THEN inst_state_type_id+1 ELSE 9 END
+			INNER JOIN (SELECT max(id) AS id, advisory_id from "ASGS_Mon_event" group by event_group_id, advisory_id) AS meid ON meid.id=e.id AND meid.advisory_id=eg.advisory_id
+			INNER JOIN (SELECT max(id) AS id, instance_id, advisory_id from "ASGS_Mon_event_group" group by instance_id, advisory_id) AS megid ON megid.id=eg.id AND megid.instance_id=i.id AND megid.advisory_id=eg.advisory_id	
+			WHERE														
+				(
+					CASE WHEN _viewActive = true
+						THEN i.inst_state_type_id IN (0, 1, 2, 4, 5, 6, 8) AND eg.state_type_id IN (0, 1, 2, 4, 5, 8)
+					END OR		
+					CASE WHEN _inactives <> ''
+						THEN i.inst_state_type_id = ANY (_inactives::int[]) OR eg.state_type_id = ANY (_inactives::int[])
+					END
+				) 
+				AND
+				(
+					CASE WHEN _sites <> ''
+						THEN s.id = ANY (_sites::int[])
+						ELSE s.id = s.id
+					END
+				)
+				AND	i.start_ts >= _since
+				AND eg.advisory_id <> '0'
 			ORDER BY i.id DESC
 		) items;
 
@@ -100,17 +163,74 @@ END;
 
 $BODY$;
 
-GRANT EXECUTE ON FUNCTION public.get_event_json() TO asgs;
+ALTER FUNCTION public.get_event_json(boolean, date, text, text)
+    OWNER TO powen;
 
-COMMENT ON FUNCTION public.get_event_json()
+GRANT EXECUTE ON FUNCTION public.get_event_json(boolean, date, text, text) TO powen;
+
+GRANT EXECUTE ON FUNCTION public.get_event_json(boolean, date, text, text) TO asgs;
+
+GRANT EXECUTE ON FUNCTION public.get_event_json(boolean, date, text, text) TO PUBLIC;
+
+COMMENT ON FUNCTION public.get_event_json(boolean, date, text, text)
     IS 'Returns a recordset (json) of the active cluster events.';
 
--- FUNCTION: public.get_init_json()
+    
+-- FUNCTION: public.get_event_msgs_by_group_json(integer, character varying)
 
--- DROP FUNCTION public.get_init_json();
+-- DROP FUNCTION public.get_event_msgs_by_group_json(integer, character varying);
+
+CREATE OR REPLACE FUNCTION public.get_event_msgs_by_group_json(
+	_event_group_id integer,
+	_advisory_id character varying)
+    RETURNS TABLE(document jsonb) 
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 1000
+AS $BODY$
+
+BEGIN
+  -- gather the records and return them in json format
+	RETURN QUERY
+	
+	SELECT jsonb_agg(r)
+	FROM (
+		SELECT to_char(e.event_ts, 'Mon DD, HH24:MI:SS') || ' - ' || e.raw_data AS event_summary	
+		FROM public."ASGS_Mon_event" e
+		WHERE e.event_group_id=_event_group_id AND e.advisory_id=_advisory_id
+		GROUP BY id 
+		ORDER BY id DESC
+		LIMIT 7) r;
+END;
+
+$BODY$;
+
+ALTER FUNCTION public.get_event_msgs_by_group_json(integer, character varying)
+    OWNER TO powen;
+
+GRANT EXECUTE ON FUNCTION public.get_event_msgs_by_group_json(integer, character varying) TO powen;
+
+GRANT EXECUTE ON FUNCTION public.get_event_msgs_by_group_json(integer, character varying) TO asgs;
+
+GRANT EXECUTE ON FUNCTION public.get_event_msgs_by_group_json(integer, character varying) TO PUBLIC;
+
+COMMENT ON FUNCTION public.get_event_msgs_by_group_json(integer, character varying)
+    IS 'Returns a recordset (json) of the last 5 event messages for a group.';
+
+    
+-- FUNCTION: public.get_init_json(boolean, date, text, text)
+
+-- DROP FUNCTION public.get_init_json(boolean, date, text, text);
 
 CREATE OR REPLACE FUNCTION public.get_init_json(
-	)
+	_viewactive boolean,
+	_since date DEFAULT to_date(
+	'01/01/1970'::text,
+	'MM/DD/YYYY'::text),
+	_inactives text DEFAULT ''::text,
+	_sites text DEFAULT ''::text)
     RETURNS TABLE(document json) 
     LANGUAGE 'plpgsql'
 
@@ -134,87 +254,115 @@ BEGIN
 				i.run_params AS run_params,
 				s.name AS title,
 				s.cluster_name AS subtitle,
+			 	eg.advisory_id AS advisory_number,
 				'' AS message,
 				ARRAY[100] AS ranges,
 				ARRAY[0] AS measures,
 				ARRAY[0] AS markers,
-				(SELECT public.get_event_msgs_by_group_json(eg.id)) AS event_raw_msgs				
+				(SELECT public.get_event_msgs_by_group_json(eg.id, eg.advisory_id)) AS event_raw_msgs
 			FROM "ASGS_Mon_instance" i
 			JOIN "ASGS_Mon_site_lu" s ON s.id=i.site_id
 			JOIN "ASGS_Mon_event_group" eg ON eg.instance_id=i.id
-			INNER JOIN (SELECT max(id) AS id, instance_id from "ASGS_Mon_event_group" group by instance_id) AS megid ON megid.id=eg.id AND megid.instance_id=i.id	
-			WHERE inst_state_type_id < CASE WHEN end_ts + INTERVAL '1 day' > now() THEN inst_state_type_id+1 ELSE 9 END
+			JOIN "ASGS_Mon_event" e on e.event_group_id=eg.id
+			INNER JOIN (SELECT max(id) AS id, advisory_id from "ASGS_Mon_event" group by event_group_id, advisory_id) AS meid ON meid.id=e.id AND meid.advisory_id=eg.advisory_id
+			INNER JOIN (SELECT max(id) AS id, instance_id, advisory_id from "ASGS_Mon_event_group" group by instance_id, advisory_id) AS megid ON megid.id=eg.id AND megid.instance_id=i.id AND megid.advisory_id=eg.advisory_id	
+			WHERE
+				(
+					CASE WHEN _viewActive = true
+						THEN i.inst_state_type_id IN (0, 1, 2, 4, 5, 6, 8) AND eg.state_type_id IN (0, 1, 2, 4, 5, 8)
+					END OR		
+					CASE WHEN _inactives <> ''
+						THEN i.inst_state_type_id = ANY (_inactives::int[]) OR eg.state_type_id = ANY (_inactives::int[])
+					END
+				) 
+				AND
+				(
+					CASE WHEN _sites <> ''
+						THEN s.id = ANY (_sites::int[])
+						ELSE s.id = s.id
+					END
+				)
+				AND	i.start_ts >= _since
+				AND eg.advisory_id <> '0'
 			ORDER BY i.id DESC
 			) AS items;
 END;
 
 $BODY$;
 
-GRANT EXECUTE ON FUNCTION public.get_init_json() TO asgs;
+ALTER FUNCTION public.get_init_json(boolean, date, text, text)
+    OWNER TO powen;
 
-COMMENT ON FUNCTION public.get_init_json()
+GRANT EXECUTE ON FUNCTION public.get_init_json(boolean, date, text, text) TO powen;
+
+GRANT EXECUTE ON FUNCTION public.get_init_json(boolean, date, text, text) TO asgs;
+
+GRANT EXECUTE ON FUNCTION public.get_init_json(boolean, date, text, text) TO PUBLIC;
+
+COMMENT ON FUNCTION public.get_init_json(boolean, date, text, text)
     IS 'Returns a recordset (json) of the running ASGS cluster instances.';
+
+-- FUNCTION: public.handle_stalled_event_groups()
+
+-- DROP FUNCTION public.handle_stalled_event_groups();
+
+CREATE OR REPLACE FUNCTION public.handle_stalled_event_groups(
+	)
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
+	DECLARE affected_rows integer;	
+BEGIN
+	/*
+	-- SQL that details the records to be updated
+	SELECT DISTINCT eg.id, eg.instance_id, e.advisory_id, i.site_id, e.event_ts,  now() - interval '3 hours' AS expiration, i.inst_state_type_id, eg.state_type_id, i.instance_name, i.run_params
+	FROM "ASGS_Mon_event_group" eg 
+	JOIN "ASGS_Mon_event" e ON e.event_group_id = eg.id
+	JOIN "ASGS_Mon_instance" i ON i.id=eg.instance_id
+	WHERE 
+		e.event_ts = (SELECT MAX(e1.event_ts) FROM "ASGS_Mon_event" e1 WHERE e1.event_group_id = eg.id AND e1.advisory_id = eg.advisory_id)
+		AND i.inst_state_type_id NOT IN (0, 3, 6, 7, 9, 10) AND eg.state_type_id NOT IN (0, 3, 6, 7, 9, 10)
+		AND e.event_ts < now() - interval '3 hours'
+		AND eg.advisory_id <> '0'
+	ORDER BY 1,2,3,4
+	*/
+
+	-- do the update
+	UPDATE "ASGS_Mon_event_group" SET state_type_id=10 WHERE ID IN 
+	(
+		SELECT DISTINCT eg.id
+		FROM "ASGS_Mon_event_group" eg 
+		JOIN "ASGS_Mon_event" e ON e.event_group_id = eg.id
+		JOIN "ASGS_Mon_instance" i ON i.id=eg.instance_id
+		WHERE 
+			e.event_ts = (SELECT MAX(e1.event_ts) FROM "ASGS_Mon_event" e1 WHERE e1.event_group_id = eg.id AND e1.advisory_id = eg.advisory_id)
+			AND i.inst_state_type_id NOT IN (0, 3, 6, 7, 9, 10) AND eg.state_type_id NOT IN (0, 3, 6, 7, 9, 10)
+			AND e.event_ts < now() - interval '3 hours'
+			AND eg.advisory_id <> '0'
+	);
+	
+	-- get the number of rows updated
+	GET DIAGNOSTICS affected_rows = ROW_COUNT;	
+	
+	-- return the count back to the caller
+	RETURN affected_rows;
+END;
+
+$BODY$;
+
+ALTER FUNCTION public.handle_stalled_event_groups()
+    OWNER TO powen;
+
+GRANT EXECUTE ON FUNCTION public.handle_stalled_event_groups() TO powen;
+
+GRANT EXECUTE ON FUNCTION public.handle_stalled_event_groups() TO asgs;
+
+GRANT EXECUTE ON FUNCTION public.handle_stalled_event_groups() TO PUBLIC;
+
+COMMENT ON FUNCTION public.handle_stalled_event_groups()
+    IS 'Updates event groups to a stalled status when no event messages have been received in 3 or more hours.';
     
-
-
--- FUNCTION: public.get_config_list_json(integer)
-
--- DROP FUNCTION public.get_config_list_json(integer);
-
-CREATE OR REPLACE FUNCTION public.get_config_list_json()
-    RETURNS TABLE(document jsonb) 
-    LANGUAGE 'plpgsql'
-
-    COST 100
-    VOLATILE 
-    ROWS 1000
-AS $BODY$
-
-BEGIN
-  -- gather the records and return them in json format
-	RETURN QUERY
-	
-	SELECT jsonb_agg(r)
-	FROM (
-		SELECT c.instance_id, i.instance_name, TO_CHAR(i.start_ts, 'Mon DD, HH24:MI:SS') AS start_ts
-		FROM public."ASGS_Mon_instance_config" c
-                JOIN "ASGS_Mon_instance" i ON i.id=c.instance_id
-		GROUP BY c.instance_id, i.instance_name, i.start_ts
-		ORDER BY c.instance_id DESC
-		LIMIT 7) r;
-END;
-
-$BODY$;
-
-
-
--- FUNCTION: public.get_config_detail_json(integer)
-
--- DROP FUNCTION public.get_config_detail_json(integer);
-
-CREATE OR REPLACE FUNCTION public.get_config_detail_json(
-	_instance_id integer)
-    RETURNS TABLE(document json) 
-    LANGUAGE 'plpgsql'
-
-    COST 100
-    VOLATILE 
-    ROWS 1000
-AS $BODY$
-
-BEGIN
-  -- gather the records and return them in json format
-	RETURN QUERY
-	
-	SELECT to_json(r)
-	FROM (
-		SELECT c.instance_id, i.instance_name, TO_CHAR(i.start_ts, 'Mon DD, HH24:MI:SS') AS start_ts, c.adcirc_config, REPLACE(REPLACE(REPLACE(c.asgs_config, '\"', '&quot;'), '''', '&apos;'), '\n', '</br>') AS asgs_config
-		FROM public."ASGS_Mon_instance_config" c
-                JOIN "ASGS_Mon_instance" i ON i.id=c.instance_id
-		WHERE c.instance_id=_instance_id
-		GROUP BY c.instance_id, i.instance_name, i.start_ts, c.adcirc_config, c.asgs_config 
-		ORDER BY c.instance_id DESC
-		LIMIT 7) r;
-END;
-
-$BODY$;
+    
