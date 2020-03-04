4ea035cca092e7521526a8996bd949ec6423f536
Phil Owen <phillipsowen@outlook.com>
2020-03-04 15:54:43 -0500

    addition of grid tab

--- ASGS_Mon/static/ASGS_Mon/monitorTab.js
+++ ASGS_Mon/static/ASGS_Mon/monitorTab.js
@@ -402,8 +402,174 @@ function renderMonitorTab(siteInstance)
 		 			
 		 	 		// reload the visualization with the new data
 		 			svg.data(eventData).call(siteInstance.duration(1500));
+		 			
+		 			refreshTable(eventData);
 				});
 			});			
  		}
  	}
+}
+
+function renderGridView(eventData)
+{
+	sortOn = true;
+	
+	var top = d3.select('#gridView').html('');
+	
+	var table = top.append('table').attr("border", 1);
+	
+	table.append('thead')
+	table.append('tbody');
+	
+	// create the table header
+	var thead = table.select("thead").selectAll("th")
+		.data(d3.keys(eventData[0]))
+		.enter().append("th").text(function(d){return d;})
+		.on("click", function(d){ return renderGridView(d);});
+
+	// fill the table	
+	var tr = table.select("tbody").selectAll("tr").data(eventData);	
+	
+	tr.enter().append("tr");
+
+	// create cells
+	var td = tr.selectAll("td").data(function(d){return d3.values(d);});	
+	
+	td.enter().append("td").text(function(d) {return d;});
+	
+	//update?
+	if(sortOn !== null) 
+	{			
+		// update rows
+		if(sortOn != previousSort)
+		{
+			tr.sort(function(a,b){return sort(a[sortOn], b[sortOn]);});
+			previousSort = sortOn;
+		}
+		else
+		{
+			tr.sort(function(a,b){return sort(b[sortOn], a[sortOn]);});
+			previousSort = null;
+		}
+		
+		//update cells
+		td.text(function(d) {return d;});
+	}
+}
+
+
+sortOn = true;
+var previousSort = null;
+
+function refreshTable(eventData)
+{	
+	var margin = {top: 20, right: 30, bottom: 30, left: 40},
+	width = 2300 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+	
+	d3.select("#gridView").html('');
+	
+	var canvas = d3.select("#gridView")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	var headerGrp = canvas.append("g").attr("class", "headerGrp");
+	var rowsGrp = canvas.append("g").attr("class","rowsGrp");
+	
+	var fieldHeight = 30;
+	var fieldWidth = 90;
+	
+	var previousSort = null;
+	var format = d3.time.format("%a %b %d %Y");
+	
+	// create the table header	
+	var header = headerGrp.selectAll("g")
+		.data(d3.keys(eventData[0]))
+		.enter().append("g")
+		.attr("class", "header")
+		.attr("transform", function (d, i){
+			return "translate(" + i * fieldWidth + ",0)";
+		})
+		.on("click", function(d){ return refreshTable(d);});
+	
+	header.append("rect")
+		.attr("width", fieldWidth-1)
+		.attr("height", fieldHeight);
+		
+	header.append("text")
+		.attr("x", fieldWidth / 2)
+		.attr("y", fieldHeight / 2)
+		.attr("dy", ".35em")
+		.text(String);
+	
+	// fill the table	
+	// select rows
+	var rows = rowsGrp.selectAll("g.row").data(eventData, function(d){ return d.eg_id; });
+	
+	// create rows	
+	var rowsEnter = rows.enter().append("svg:g")
+		.attr("class","row")
+		.attr("transform", function (d, i){
+			return "translate(0," + (i+1) * (fieldHeight+1) + ")";
+		});
+	
+	// select cells
+	var cells = rows.selectAll("g.cell").data(function(d){return d3.values(d);});
+	
+	// create cells
+	var cellsEnter = cells.enter().append("svg:g")
+		.attr("class", "cell")
+		.attr("transform", function (d, i){
+			return "translate(" + i * fieldWidth + ",0)";
+		});
+		
+	cellsEnter.append("rect")
+		.attr("width", fieldWidth-1)
+		.attr("height", fieldHeight);	
+		
+	cellsEnter.append("text")
+		.attr("x", fieldWidth / 2)
+		.attr("y", fieldHeight / 2)
+		.attr("dy", ".35em")
+		.text(String);
+	
+	//update if not in initialization
+	if(sortOn !== null) {
+			// update rows
+			if(sortOn != previousSort){
+				rows.sort(function(a,b){return sort(a[sortOn], b[sortOn]);});			
+				previousSort = sortOn;
+			}
+			else{
+				rows.sort(function(a,b){return sort(b[sortOn], a[sortOn]);});
+				previousSort = null;
+			}
+			rows.transition()
+				.duration(500)
+				.attr("transform", function (d, i){
+					return "translate(0," + (i+1) * (fieldHeight+1) + ")";
+				});
+	}
+}
+	
+function sort(a,b)
+{
+	if(typeof a == "string"){
+		var parseA = format.parse(a);
+		if(parseA){
+			var timeA = parseA.getTime();
+			var timeB = format.parse(b).getTime();
+			return timeA > timeB ? 1 : timeA == timeB ? 0 : -1;
+		}
+		else 
+			return a.localeCompare(b);
+	}
+	else if(typeof a == "number"){
+		return a > b ? 1 : a == b ? 0 : -1;
+	}
+	else if(typeof a == "boolean"){
+		return b ? 1 : a ? -1 : 0;
+	}
 }
