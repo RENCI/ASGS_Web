013deef74bbf2eeb1c51521a6f86ccd40fb936f9
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-08-15 12:59:12 -0400

    adding a wait period before starting the queue handlers to give the MQ to fully initialize. the wait parameter is configurable in the k8s deploy

--- messages/startup.sh
+++ messages/startup.sh
@@ -1,5 +1,6 @@
 #/usr/bin/bash
-
+echo "sleeping $1 seconds to give the queue time to initialize"
+sleep $1
 python receive_msg_service_pg.py &
 python receive_cfg_msg_service_pg.py &
 while true; do sleep 600; done;
