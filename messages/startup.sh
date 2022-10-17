946433f14c89dbd91f2a9db9181d31d8f4bcde6e
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-10-17 15:35:09 -0400

    making sure startup.sh has execute permissions. adding comment to startup file.

--- messages/startup.sh
+++ messages/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "sleeping $1 second(s) to give the queue time to initialize"
+echo "Sleeping $1 second(s) to give the queue time to initialize"
 sleep $1
+echo "Starting message handlers..."
 python receive_msg_service_pg.py &
 python receive_cfg_msg_service_pg.py &
 while true; do sleep 600; done;
